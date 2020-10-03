8b85a81f471a12ac7470a822e58b5849c056c9a3
Andrew Lawler <alawler6@gmail.com>
2020-10-03 14:14:51 -0400

    Wire up sliders; add 'Pick new color' btn

--- screenshot.js
+++ screenshot.js
@@ -1,23 +1,46 @@
-var colorcapture_active = true;
+var colorCaptureActive = true;
+var imgData = null;
+var hexColor = null;
 
-chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
+var colorPreview = null;
+var colorPreviewHex = null;
+var sliderR = null;
+var sliderB = null;
+var sliderG = null;
+var btnPickNew = null;
+
+chrome.extension.onMessage.addListener (function(msg, sender, sendResponse) {
     updateImgSrc(msg.url);
 });
 
-function getCursorPosition(canvas, event) {
+function getCursorPosition (canvas, event) {
     const rect = canvas.getBoundingClientRect()
     const x = event.clientX - rect.left
     const y = event.clientY - rect.top
     return [x, y];
 }
 
-function rgbToHex(r, g, b) {
+function pickColor () {
+    colorCaptureActive = false;
+    btnPickNew.disabled = false;
+}
+
+function rgbToHex (r, g, b) {
     if (r > 255 || g > 255 || b > 255)
         throw "Invalid color component";
     return ((r << 16) | (g << 8) | b).toString(16);
 }
 
-function updateImgSrc(url) {
+function updateColor () {
+    r = parseInt(sliderR.value)
+    g = parseInt(sliderG.value)
+    b = parseInt(sliderB.value)
+    hexColor = "#" + ("000000" + rgbToHex(r, g, b)).slice(-6);
+    colorPreview.style.backgroundColor = hexColor;
+    colorPreviewHex.textContent = hexColor;
+}
+
+function updateImgSrc (url) {
 
     var image = document.getElementById("temp");
     var canvas = document.getElementById("main-canvas");
@@ -37,21 +60,32 @@ function updateImgSrc(url) {
         ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, image.width * ratio, image.height * ratio);
 
         image.remove();
+
+        canvas.addEventListener('click', pickColor);
+        colorPreview = document.getElementById('color-preview');
+        colorPreviewHex = document.getElementById('color-preview-hex');
+        sliderR = document.getElementById('range-red');
+        sliderR.oninput = updateColor;
+        sliderG = document.getElementById('range-green');
+        sliderG.oninput = updateColor;
+        sliderB = document.getElementById('range-blue');
+        sliderB.oninput = updateColor;
+        btnPickNew = document.getElementById('btn-pick-new');
     }
 
     image.src = url;
 
     document.body.addEventListener('mousemove', (e) => {
-        if (colorcapture_active) {
+        if (colorCaptureActive) {
             mouse_pos = getCursorPosition(canvas, e);
-            var imgData = window.ctx.getImageData(mouse_pos[0], mouse_pos[1], 1, 1).data;
-            var hex = "#" + ("000000" + rgbToHex(imgData[0], imgData[1], imgData[2])).slice(-6);
-            document.getElementById('color-preview').style.backgroundColor = hex;
-            document.getElementById('color-preview-hex').textContent = hex;
+            if (mouse_pos[0] > 0 && mouse_pos[1] > 0) {
+                imgData = window.ctx.getImageData(mouse_pos[0], mouse_pos[1], 1, 1).data;                
+                sliderR.value = imgData[0].toString();
+                sliderG.value = imgData[1].toString();
+                sliderB.value = imgData[2].toString();
 
-            document.getElementById('range-red').value = imgData[0].toString();
-            document.getElementById('range-green').value = imgData[1].toString();
-            document.getElementById('range-blue').value = imgData[2].toString();
+                updateColor();
+            }
         }
     });
 }
